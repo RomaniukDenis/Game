7a517c98e8a05fec203aa65ec77fdaf5a54edf4c
denis romaniuk <den.romaniuk.2010@gmail.com>
2023-01-22 19:16:36 +0200

    i

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import io from 'socket.io-client';
 import Game from "./components/game/Game";
 
-const socket = io.connect("http://localhost:5000");
+const socket = io.connect(window.location);
 
 function App() {
   return (
